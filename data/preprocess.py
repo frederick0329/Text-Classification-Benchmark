2a1db8d8c3f4c09aa78f282bb6489564d72ef671
Frederick Liu <frederick0329@gmail.com>
2017-08-20 18:37:53 -0700

    dataset

---
+++ data/preprocess.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+import csv
+import re
+
+def preprocess_ag_news():
+    raw_train_file = './raw/ag_news_csv/train.csv' 
+    raw_test_file = './raw/ag_news_csv/test.csv'
+    train_file = './preprocessed/ag_news_train.txt'
+    test_file = './preprocessed/ag_news_test.txt'
+    f_train =  open(train_file,'w')
+    f_test =  open(test_file,'w')
+
+
+    with open(raw_train_file, 'r') as f:
+        reader = csv.reader(f, delimiter=',', quotechar='"')
+        for row in reader:
+            txt = ""
+            for s in row[1:]:
+                txt = txt + " " + re.sub("^\s*(.-)\s*$", "%1", s).replace("\\n", "\n")
+            f_train.write(row[0] + '\t' + txt + '\n')
+    f_train.close()
+    
+    with open(raw_test_file, 'r') as f:
+        reader = csv.reader(f, delimiter=',', quotechar='"')
+        for row in reader:
+            txt = ""
+            for s in row[1:]:
+                txt = txt + " " + re.sub("^\s*(.-)\s*$", "%1", s).replace("\\n", "\n")
+            f_test.write(row[0] + '\t' + txt + '\n')
+    f_test.close()
+
+
+preprocess_ag_news()
